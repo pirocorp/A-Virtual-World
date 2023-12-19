886498325dfb397f904994ad4ff10a43905c354f
Zdravko Zdravkov <zdr0@yahoo.com>
2023-12-19 17:20:14 +0100

    Add additional segment

--- World Editor/src/index.ts
+++ World Editor/src/index.ts
@@ -16,8 +16,9 @@ const p4 = new Point(100, 300);
 const s1 = new Segment(p1, p2);
 const s2 = new Segment(p1, p3);
 const s3 = new Segment(p1, p4);
+const s4 = new Segment(p2, p3);
 
-const graph: IGraph = new Graph([p1, p2, p3, p4], [s1, s2, s3]);
+const graph: IGraph = new Graph([p1, p2, p3, p4], [s1, s2, s3, s4]);
 graph.draw(ctx);
 
 
